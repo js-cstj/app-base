f253343fe90952879ba799d289ea9a0999db5fdd
Martin Boudreau <mboudrea@cstj.qc.ca>
2025-11-06 11:22:54 -0500

    2025

--- src/App.js
+++ src/App.js
@@ -5,5 +5,16 @@ export default class App {
 	 */
 	static main() {
 		var app = document.getElementById("app");
+		this.chargerJson("fichier.json").then(donnees => {
+			console.log(donnees);
+		});
+	}
+	/**
+	 * Charge un fichier JSON.
+	 * @param {string} url URL du fichier JSON ou de l'API
+	 * @returns {Promise} Promise résolue avec le JSON
+	 */
+	static chargerJson(url) {
+		return fetch(url).then(reponse => reponse.json());
 	}
 }
